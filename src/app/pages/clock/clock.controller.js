718a5830910a6b82d34f1255df1bfb7398da2199
keligijus <k.eligijus@gmail.com>
2016-02-28 13:46:58 +0200

    completed checkbox added

--- src/app/pages/clock/clock.controller.js
+++ src/app/pages/clock/clock.controller.js
@@ -8,34 +8,13 @@
   /** @ngInject */
   function controller($log, trelloFactory, resolveGetChecklistItem) {
     var vm = this;
-    //     vm.cards = resolveGetBoardCards;
         vm.trello = trelloFactory;
-        // vm.item = vm.trello.current.checklist;
-
         vm.item = resolveGetChecklistItem;
 
-        $log.debug(vm.item)
+        $log.debug(vm.item);
 
-    // $log.debug(resolveGetBoardCards);
+        vm.completed = vm.item.state === 'complete' ? true : false;
 
-    // vm.init = function() {
-    //   vm.getChecklists(vm.cards);
-    // }
-
-
-    // vm.getChecklists = function(cards) {
-    //   cards.forEach(function(card, index){
-    //     var checklists = [];
-    //     card.idChecklists.forEach(function(checklistId){
-    //         vm.trello.getChecklist(checklistId).then(function(result){
-    //           checklists.push(result);
-    //         });
-    //       });
-    //     return card.returnedChecklists = checklists;
-    //   });
-    // }
-
-    // vm.init();
 
   } //
 })();
