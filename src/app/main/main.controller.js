220125b213241ed485e3aed4548aa3cb837ef7f3
keligijus <k.eligijus@gmail.com>
2016-03-05 22:39:17 +0200

    applying homepage ux

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,6 +10,7 @@
     var vm = this;
 
     vm.trello = trelloFactory;
+    vm.boardsSearch = undefined;
 
     // activate();
 
@@ -27,30 +28,13 @@
       });
     }
 
+    vm.goToSelectedBoards = function() {
+      vm.boards.forEach(function(board) {
+        if (board.TTisSelected) {
+          
+        }
+      });
+    }
 
-
-
-
-
-
-    // function activate() {
-    //   getWebDevTec();
-    //   $timeout(function() {
-    //     vm.classAnimation = 'rubberBand';
-    //   }, 4000);
-    // }
-
-    // function showToastr() {
-    //   toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    //   vm.classAnimation = '';
-    // }
-
-    // function getWebDevTec() {
-    //   vm.awesomeThings = webDevTec.getTec();
-
-    //   angular.forEach(vm.awesomeThings, function(awesomeThing) {
-    //     awesomeThing.rank = Math.random();
-    //   });
-    // }
   }
 })();
